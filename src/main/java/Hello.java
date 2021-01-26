7a48986a5519a813807fe7353240dbd4cf2399e4
Sunnat Davletov <Davletovnyc@gmail.com>
2021-01-26 13:43:37 -0500

    B21 learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("B21 learning git future SDET");
+    }
 }
