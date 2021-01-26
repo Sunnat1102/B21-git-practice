c714a85c1801312d83ca0689e4d2bad2e07faf41
Sunnat Davletov <Davletovnyc@gmail.com>
2021-01-26 14:22:31 -0500

    Started working on FutureAcode

---
+++ src/test/java/FutureACode.java
@@ -0,0 +1,5 @@
+public class FutureACode {
+    public static void main(String[] args) {
+        System.out.println("AAwesome");
+    }
+}
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,3 @@
 public class HelloTest {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    //Testing my test
     }
-}
